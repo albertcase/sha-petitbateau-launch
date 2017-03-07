3f7e4973dd279777a8b65095b4c98fa19bdc7952
Demon Zhang <demon.zhang@samesamechina.com>
2017-03-07 14:37:47 +0800

    no message

---
+++ config/config.php
@@ -0,0 +1,60 @@
+<?php
+
+define("BASE_URL", 'http://127.0.0.1:9211/');
+define("TEMPLATE_ROOT", dirname(__FILE__) . '/../template');
+define("VENDOR_ROOT", dirname(__FILE__) . '/../vendor');
+
+//User
+define("USER_STORAGE", 'COOKIE');
+
+//Wechat Vendor
+define("WECHAT_VENDOR", 'curio'); // default | curio
+
+//Wechat config info
+define("TOKEN", '?????');
+define("APPID", '?????');
+define("APPSECRET", '?????');
+define("NOWTIME", date('Y-m-d H:i:s'));
+define("AHEADTIME", '100');
+
+define("NONCESTR", '?????');
+define("CURIO_AUTH_URL", '?????'); 
+
+//Redis config info
+define("REDIS_HOST", '127.0.0.1');
+define("REDIS_PORT", '6379');
+
+//Database config info
+define("DBHOST", '127.0.0.1');
+define("DBUSER", 'root');
+define("DBPASS", '');
+define("DBNAME", 'petitbateau-launch');
+
+//Wechat Authorize
+define("CALLBACK", 'wechat/callback');
+define("SCOPE", 'snsapi_base');
+
+//Wechat Authorize Page
+define("AUTHORIZE_URL", '[
+	"/"
+]');
+
+//Account Access
+define("OAUTH_ACCESS", '{
+	"xxxx": "samesamechina.com" 
+}');
+define("JSSDK_ACCESS", '{
+	"xxxx": "samesamechina.com"
+}');
+
+define("ENCRYPT_KEY", '29FB77CB8E94B358');
+define("ENCRYPT_IV", '6E4CAB2EAAF32E90');
+
+define("WECHAT_TOKEN_PREFIX", 'wechat:token:');
+
+
+
+
+
+
+
---
+++ config/router.php
@@ -0,0 +1,14 @@
+<?php
+
+$routers = array();
+$routers['/wechat/oauth2'] = array('WechatBundle\Wechat', 'oauth');
+$routers['/wechat/callback'] = array('WechatBundle\Wechat', 'callback');
+$routers['/wechat/curio/callback'] = array('WechatBundle\Curio', 'callback');
+$routers['/wechat/curio/receive'] = array('WechatBundle\Curio', 'receiveUserInfo');
+$routers['/wechat/ws/jssdk/config/webservice'] = array('WechatBundle\WebService', 'jssdkConfigWebService');
+$routers['/wechat/ws/jssdk/config/js'] = array('WechatBundle\WebService', 'jssdkConfigJs');
+$routers['/ajax/post'] = array('CampaignBundle\Api', 'form');
+$routers['/'] = array('CampaignBundle\Page', 'index');
+$routers['/clear'] = array('CampaignBundle\Page', 'clearCookie');
+$routers['/apply'] = array('CampaignBundle\Page', 'apply');
+$routers['/result'] = array('CampaignBundle\Page', 'result');
