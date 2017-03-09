d34961c9ba95cda7dfbe0681a3c1e7b9acb99fc2
vincenwen <vincen.wen@samesamechina.com>
2017-03-09 16:16:28 +0800

    add debug=true;

--- template/result.tpl.php
+++ template/result.tpl.php
@@ -18,7 +18,7 @@
     <meta name="Keywords" content="">
     <meta name="Description" content="...">
     <link rel="stylesheet" type="text/css" href="/build/assets/css/main.min.css">
-    <script type="text/javascript" src="http://pbwechat.samesamechina.com/api/v1/js/4c360e05-6e2e-465f-8583-9c247cb9465c/wechat"></script>
+    <script type="text/javascript" src="http://pbwechat.samesamechina.com/api/v1/js/4c360e05-6e2e-465f-8583-9c247cb9465c/wechat?debug=true"></script>
 </head>
 <body>
 <div class="loading" >
