2d6fe970679baf17afeaff0d2ad3e2db956c4d1b
vincenwen <vincen.wen@samesamechina.com>
2017-03-09 15:34:03 +0800

    ;

--- template/index.tpl.php
+++ template/index.tpl.php
@@ -18,6 +18,7 @@
     <meta name="Keywords" content="">
     <meta name="Description" content="...">
     <link rel="stylesheet" type="text/css" href="/build/assets/css/main.min.css">
+    <script type="text/javascript" src="http://pbwechat.samesamechina.com/api/v1/js/4c360e05-6e2e-465f-8583-9c247cb9465c/wechat"></script>
 </head>
 <body>
 <div class="loading" >
@@ -120,9 +121,8 @@
     
     pfun.loadingFnDoing(allimg, function(){
         $(".loading").css({"visibility": "hidden"});
-        // pfun.overscroll(document.querySelector("#chatArea")); 
         _v.sectionChange("index");
-        // pfun.init();
+        pfun.init();
 
         // Setup FastClick.
         FastClick.attach(document.body);
